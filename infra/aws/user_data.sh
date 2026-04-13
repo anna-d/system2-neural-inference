a5ad6958cd5d6c2ce91d6ca1bd6713441db2f930
Anna Damtsa <andamtsa@gmail.com>
2026-04-13 18:57:03 +0100

    Refactor EC2 infrastructure setup
    - Convert launch template to CLI-compatible format
    - Add Spot instance configuration
    - Ensure repo is always up-to-date on startup
    - Fix file ownership and working directory issues

--- infra/aws/user_data.sh
+++ infra/aws/user_data.sh
@@ -7,7 +7,12 @@ if [ ! -d system2-neural-inference ]; then
   git clone https://github.com/anna-d/system2-neural-inference.git
 fi
 
-cd system2-neural-inference
+cd /home/ec2-user/system2-neural-inference
+
+git fetch origin
+git reset --hard origin/main
 
 source /opt/pytorch/bin/activate || true
 pip install -r requirements.txt
+
+chown -R ec2-user:ec2-user /home/ec2-user/system2-neural-inference
